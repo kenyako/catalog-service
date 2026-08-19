7d05f7a29f53a8e98b6c22c178e0bcc1d12bae89
osd <osd@kode.ru>
2026-08-19 22:53:29 +0700

    TASK-1003: Реализовала клиент PostgreSQL с проверкой соединения и встроенной системой миграций

--- internal/app/config/section/repository.go
+++ internal/app/config/section/repository.go
@@ -8,11 +8,13 @@ type (
 	}
 
 	RepositoryPostgres struct {
-		Address      string        `required:"true"`
-		Username     string        `required:"true"`
-		Password     string        `required:"true"`
-		Name         string        `required:"true"`
-		ReadTimeout  time.Duration `split_words:"true" default:"30s"`
-		WriteTimeout time.Duration `split_words:"true" default:"30s"`
+		MigrationTable string        `split_words:"true" default:"schema_migrations"`
+		Address        string        `required:"true"`
+		Username       string        `required:"true"`
+		Password       string        `required:"true"`
+		Name           string        `required:"true"`
+		ReadTimeout    time.Duration `split_words:"true" default:"30s"`
+		WriteTimeout   time.Duration `split_words:"true" default:"30s"`
+		DialTimeout    time.Duration `split_words:"true" default:"5s"`
 	}
 )
