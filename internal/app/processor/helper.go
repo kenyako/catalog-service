bcadcb46aa94f1f669fe4f845f1a45429385da04
osd <osd@kode.ru>
2026-09-04 00:57:15 +0700

    TASK-2004: Добавила graceful shutdown и web-server

--- internal/app/processor/helper.go
+++ internal/app/processor/helper.go
@@ -2,8 +2,44 @@ package processor
 
 import (
 	"context"
+	"io"
 	"sync"
+	"time"
 )
+
+type (
+	CloserFunc        func() error
+	CloserContextFunc = func(ctx context.Context) error
+)
+
+func (f CloserFunc) Close() error {
+	return f()
+}
+
+func NewCloserContextFunc(f CloserContextFunc, ctx context.Context, timeout time.Duration) CloserFunc {
+	return func() error {
+		if timeout > 0 {
+			var cancelFunc context.CancelFunc
+
+			ctx, cancelFunc = context.WithTimeout(ctx, timeout)
+			defer cancelFunc()
+		}
+
+		return f(ctx)
+	}
+}
+
+func WatchForShutdown(ctx context.Context, wg *sync.WaitGroup, closer io.Closer) {
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+
+		<-ctx.Done()
+
+		_ = closer.Close()
+	}()
+}
 
 func Wrap(ctx context.Context, wg *sync.WaitGroup, cb func(context.Context)) {
 	if wg != nil {
--- internal/app/processor/http/http.go
+++ internal/app/processor/http/http.go
@@ -1,14 +1,19 @@
 package rprocessor
 
 import (
+	"context"
 	"fmt"
+	"net"
 	"net/http"
+	"sync"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/rs/zerolog/log"
 
 	"github.com/kenyako/catalog-service/internal/app/config/section"
 	rhandler "github.com/kenyako/catalog-service/internal/app/handler/http"
+	"github.com/kenyako/catalog-service/internal/app/processor"
 	"github.com/kenyako/catalog-service/internal/app/util"
 	"github.com/kenyako/catalog-service/internal/pkg/http/httph"
 	"github.com/kenyako/catalog-service/internal/pkg/http/mzerolog"
@@ -24,7 +29,7 @@ func NewHTTP(
 	hCategory rhandler.Category,
 	hProduct rhandler.Product,
 	cfg section.ProcessorWebServer,
-) *httpProc {
+) processor.Processor {
 	r := mux.NewRouter()
 
 	r.NotFoundHandler = http.HandlerFunc(handlerNotFound)
@@ -65,16 +70,36 @@ func NewHTTP(
 	})
 
 	p := httpProc{addr: fmt.Sprintf(":%d", cfg.ListenPort)}
-	p.server.Addr = p.addr
 	p.server.Handler = r
 
 	return &p
 }
 
-func (p *httpProc) Serve() error {
-	log.Info().
-		Str("addr", p.addr).
-		Msg("Starting HTTP server")
+func (p *httpProc) StartAsync(ctx context.Context, wg *sync.WaitGroup) {
+	lc := net.ListenConfig{}
 
-	return p.server.ListenAndServe()
+	l, err := lc.Listen(ctx, "tcp", p.addr)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to listen")
+	}
+
+	log.Info().Str("addr", p.addr).Msg("HTTP server started")
+
+	go p.serve(l)
+
+	processor.WatchForShutdown(
+		ctx,
+		wg,
+		processor.CloserFunc(l.Close),
+	)
+
+	processor.WatchForShutdown(
+		ctx,
+		wg,
+		processor.NewCloserContextFunc(p.server.Shutdown, ctx, 5*time.Second),
+	)
+}
+
+func (p *httpProc) serve(l net.Listener) {
+	_ = p.server.Serve(l)
 }
